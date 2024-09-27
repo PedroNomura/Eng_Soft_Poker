e4bae6c988cb08d545e945926560b19cf7b4853c
Victor-Vaglieri <127432508+Victor-Vaglieri@users.noreply.github.com>
2024-09-27 14:41:45 -0300

    Update test_poker-v0.py

--- test_poker-v0.py
+++ test_poker-v0.py
@@ -1,5 +1,5 @@
 import pytest
-from poker import criar_partida, listar_partidas, iniciar_partida, realizar_acao
+from poker_g import criar_partida, listar_partidas, iniciar_partida, realizar_acao
 
 def test_criar_partida():
     sala = criar_partida("Teste Poker", big_blind=50, small_blind=25)
