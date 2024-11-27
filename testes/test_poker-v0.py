c00826c4836a868eabda26d244e14780b979ac04
Victor <victor_vglr@hotmail.com>
2024-11-27 13:09:19 -0300

    arrumei os diretorios

---
+++ testes/test_poker-v0.py
@@ -0,0 +1,59 @@
+import pytest
+import requests
+
+# Certifique-se de que o código das classes Player, PokerRoom e funções estejam no arquivo poker.py
+from pokerV0 import Player, PokerRoom, salas, listar_partidas, iniciar_partida
+
+# Teste para criar uma sala e garantir que o jogador seja adicionado corretamente
+def test_criar_sala():
+    jogador = Player('victor', 1000)
+    sala = jogador.criar_sala('sala1', 5, 10)
+    # Verifica se a sala foi criada corretamente
+    assert sala.nome == 'sala1'
+    assert sala.small_blind == 5
+    assert sala.big_blind == 10
+    assert len(sala.players) == 1
+    assert jogador in sala.players
+    assert jogador.indice_sala == 0
+
+# Teste para entrar em uma sala existente
+def test_entrar_sala():
+    jogador2 = Player('joao', 2000)
+    entrou = jogador2.entrar_sala('sala1')
+    
+    assert entrou == True
+    assert jogador2 in salas[0].players
+
+# Teste para coletar cartas de um jogador
+def test_coletar_cartas():
+    jogador = Player('victor', 1000)
+    jogador.coletar_cartas('mmtzxpyktzir')  # Use um ID de baralho válido se necessário
+    
+    assert len(jogador.cards) == 2
+    assert isinstance(jogador.cards[0], dict)  # Verifica se as cartas são dicionários
+    assert 'value' in jogador.cards[0]  # Verifica se a carta possui a chave 'value'
+    requests.get(f"https://deckofcardsapi.com/api/deck/mmtzxpyktzir/return/")
+    requests.get(f"https://deckofcardsapi.com/api/deck/mmtzxpyktzir/shuffle/")
+
+
+# Teste para iniciar uma partida
+def test_iniciar_partida():
+    # Inicia a partida
+    partida_iniciada = iniciar_partida(0)
+    
+    assert partida_iniciada == True
+    assert len(salas[0].players) == 2
+    assert len(salas[0].flop) == 3  # Verifica se o flop foi distribuído
+    assert len(salas[0].turn) == 1  # Verifica se o turn foi distribuído
+    assert len(salas[0].river) == 1  # Verifica se o river foi distribuído
+
+# Teste para listar as partidas
+def test_listar_partidas():
+    jogador = Player('victor', 1000)
+    jogador.criar_sala('sala2', 5, 10)
+    
+    partidas = listar_partidas()
+    
+    assert 2 == len(partidas)
+    assert partidas[1].nome == 'sala2'
+
